4c7d26e1fc50d4562c41b9bd12b88b73ba1471c5
chouchou <chaudiemhoang2@gmail.com>
2023-07-12 16:10:47 +0700

    Update PolypSegmentation_app.py

--- PolypSegmentation_app.py
+++ PolypSegmentation_app.py
@@ -2,15 +2,9 @@ import streamlit as st
 import tensorflow as tf
 import numpy as np
 from PIL import Image
+from train import iou
 
-def iou(y_true, y_pred):
-    def f(y_true, y_pred):
-        intersection = (y_true * y_pred).sum()
-        union = y_true.sum() + y_pred.sum() - intersection
-        x = (intersection + 1e-15) / (union + 1e-15)
-        x = x.astype(np.float32)
-        return x
-    return tf.numpy_function(f, [y_true, y_pred], tf.float32)
+
 
 def run(image, model):
     pre_mask = model.predict(np.expand_dims(image, axis=0))[0] > 0.5
