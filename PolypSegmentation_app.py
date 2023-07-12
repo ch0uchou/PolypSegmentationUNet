2d0af0daf344d1200d189b68c3e91c8e396cba0b
chouchou <chaudiemhoang2@gmail.com>
2023-07-12 18:49:38 +0700

    Update PolypSegmentation_app.py

--- PolypSegmentation_app.py
+++ PolypSegmentation_app.py
@@ -32,7 +32,7 @@ if file is not None:
 
     pre_image = model.predict(np.expand_dims(image, axis=0))[0] > 0.5
     
-    final_pre = np(mask_parse(pre_image))
+    final_pre = np.concatenate(mask_parse(pre_image))
 
     final_image = cv2.imwrite("final.png",final_pre)
     st.image(final_image)
