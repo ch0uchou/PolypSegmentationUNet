9e2d5f471c0c1b4a64c7352b7282f13d35e73c67
chouchou <chaudiemhoang2@gmail.com>
2023-07-12 18:17:51 +0700

    Update PolypSegmentation_app.py

--- PolypSegmentation_app.py
+++ PolypSegmentation_app.py
@@ -3,7 +3,15 @@ import tensorflow as tf
 from tensorflow.keras.utils import CustomObjectScope
 import numpy as np
 from PIL import Image
-from train import iou
+
+def iou(y_true, y_pred):
+    def f(y_true, y_pred):
+        intersection = (y_true * y_pred).sum()
+        union = y_true.sum() + y_pred.sum() - intersection
+        x = (intersection + 1e-15) / (union + 1e-15)
+        x = x.astype(np.float32)
+        return x
+    return tf.numpy_function(f, [y_true, y_pred], tf.float32)
 
 def read_image(image):
     newsize = (256,256)
