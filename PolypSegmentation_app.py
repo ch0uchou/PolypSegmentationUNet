4d594803e04437ac339e44be427fa33a598e9314
chouchou <chaudiemhoang2@gmail.com>
2023-07-12 18:43:13 +0700

    Update PolypSegmentation_app.py

--- PolypSegmentation_app.py
+++ PolypSegmentation_app.py
@@ -28,16 +28,19 @@ if file is not None:
     image = cv2.resize(image, (256, 256))
     image = image/255.0
 
+    st.image(image)
+
     pre_image = model.predict(np.expand_dims(image, axis=0))[0] > 0.5
     
     h, w, _ = image.shape
     white_line = np.ones((h, 10, 3)) * 255.0
     
     all_images = [
-        image, white_line,
-        mask_parse(pre_image) 
+        image * 255.0, white_line,
+        mask_parse(pre_image) * 255.0
     ]
 
     final_image = np.concatenate(all_images, axis=1)
+    
     st.write("## Prediction Mask")
-    st.image(final_image)
+    # st.image(final_image)
